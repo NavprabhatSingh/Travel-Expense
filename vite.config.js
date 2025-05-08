ccd11349eeae41d1f552793ed2354612e99cca05
NavprabhatSingh <navprabhatsingh252@gmail.com>
2025-05-08 17:49:02 +0530

    Prepare for GitHub Pages deploy

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Travel-Expense/',  // ⚠️ This must match your GitHub repo name exactly
   plugins: [
-    react(),tailwindcss()
+    react(),
+    tailwindcss(),
   ],
-})
+});
